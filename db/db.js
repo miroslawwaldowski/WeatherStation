147a9ae40222e17a5e8f170fa3813373a7eed9bc
Mirosław Waldowski <mwa@amu.edu.pl>
2020-10-02 12:38:47 +0200

    pool fix

--- db/db.js
+++ db/db.js
@@ -1,13 +1,10 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const isProduction = process.env.NODE_ENV === "production"
-
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  connectionString: connectionString,
-  ssl: isProduction,
+  connectionString: connectionString
 });
 
 module.exports = pool;
