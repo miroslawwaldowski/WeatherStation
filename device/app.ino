65663d3378c3d1583083bd163913163b2840ed78
Mirosław Waldowski <mwa@amu.edu.pl>
2020-12-17 00:27:32 +0100

    add GSM and location

--- device/app.ino
+++ device/app.ino
@@ -3,33 +3,53 @@
 #include <NovaSDS011.h>
 #include <SoftwareSerial.h>
 
-//test libraly
+//dev
 #include <MemoryUsage.h>
 
-#define UV A7
-#define BATTERY A6
-#define SDS_PIN_RX 5
-#define SDS_PIN_TX 6
-#define ESP_PIN_RX 3
-#define ESP_PIN_TX 4
+//------------------System configuration--------------------------
+/*If DEBUG is true, it prints answers from the modem to Serial*/
 #define DEBUG true
-
-//Server info example server: mwa-weather-station.herokuapp.com  ; potr: 80; url: /post
-const char server[] PROGMEM = {"*********"};
+/*If GSM is true, connect via GSM/GPRS, if false, connect via WiFi*/
+#define GSM_MODE true
+//Server info
+const char server[] PROGMEM = {"mwa-weather-station.herokuapp.com"};
 int port = 80;
 const char url[] PROGMEM = {"/post"};
-
 //Wi-Fi
-const char ssid_WiFI[] PROGMEM = {"*********"};
-const char password_WiFI[] PROGMEM = {"*********"};
+const char ssid_WiFI[] PROGMEM = {"PLAY_ONLINE_2212"};
+const char password_WiFI[] PROGMEM = {"Zacisze1a5"};
+//GPRS APN - PLAY
+const char gprs_apn[] PROGMEM = {"\"INTERNET\""};
+//device info
+const char device_name[] PROGMEM = {"prototyp"};
+const char server_password[] PROGMEM = {"haslo"};
+//---------------------------------------------------------------
 
-//device info example device_name: prototyp  ; server_password: haslo
-const char device_name[] PROGMEM = {"*********"};
-const char server_password[] PROGMEM = {"*********"};
-
-//AT Commands
+//AT Commands WIFI
 const char reset_WiFi[] PROGMEM = {"AT+RST"};
 const char connecting_WiFi[] PROGMEM = {"AT+CWJAP_CUR=\""};
+
+//AT Commands GSM
+const char handshake[] PROGMEM = {"AT"};
+const char signal_test[] PROGMEM = {"AT+CSQ"};
+const char network_test[] PROGMEM = {"AT+CREG?"};
+const char attach_GPRS[] PROGMEM = {"AT+CGATT=1"};
+const char set_bearer_profile[] PROGMEM = {"AT+SAPBR =3,1,CONTYPE,GPRS"};
+const char set_bearer_apn[] PROGMEM = {"AT+SAPBR =3,1,APN,"};
+const char open_bearer[] PROGMEM = {"AT+SAPBR=1,1"};
+const char query_bearer[] PROGMEM = {"AT+SAPBR=2,1"};
+const char get_location[] PROGMEM = {"AT+CLBS=1,1"};
+const char close_bearer[] PROGMEM = {"AT+SAPBR=0,1"};
+const char detach_GPRS[] PROGMEM = {"AT+CGATT=0"};
+const char cip_status[] PROGMEM = {"AT+CIPSTATUS"};
+const char cip_mux[] PROGMEM = {"AT+CIPMUX=0"};
+const char set_PDP[] PROGMEM = {"AT+CGDCONT=1,\"IP\","};
+const char cip_apn[] PROGMEM = {"AT+CSTT="};
+const char cip_gprs[] PROGMEM = {"AT+CIICR"};
+const char cip_ip[] PROGMEM = {"AT+CIFSR"};
+const char cip_exit_config[] PROGMEM = {"AT+CIPSHUT"};
+
+//AT Commands TCP
 const char start_TCP_connection[] PROGMEM = {"AT+CIPSTART=\"TCP\",\""};
 const char send_over_TCP[] PROGMEM = {"AT+CIPSEND="};
 
@@ -43,6 +63,8 @@ const char JSON_pm10[] PROGMEM = {",\"pm10\":"};
 const char JSON_uv[] PROGMEM = {",\"uv\":"};
 const char JSON_device_name[] PROGMEM = {",\"name\":\""};
 const char JSON_server_password[] PROGMEM = {"\",\"password\":\""};
+const char JSON_latitude[] PROGMEM = {",\"latitude\":"};
+const char JSON_longitude[] PROGMEM = {",\"longitude\":"};
 
 //HTML POST header
 const char HTML_POST_header_1[] PROGMEM = {"POST "};
@@ -54,6 +76,20 @@ const char HTML_POST_header_5[] PROGMEM = {"Content-Length: "};
 String r = "\r";
 String n = "\n";
 
+#define UV A7
+#define BATTERY A6
+#define SDS_PIN_RX 5
+#define SDS_PIN_TX 6
+#if GSM_MODE
+//SIM800
+#define WWW_PIN_RX 8
+#define WWW_PIN_TX 7
+#else
+//ESP
+#define WWW_PIN_RX 3
+#define WWW_PIN_TX 4
+#endif
+
 //measurement data
 float temperature;
 float humidity;
@@ -61,8 +97,8 @@ float pressure;
 byte uv;
 float pm10;
 float pm25;
-float latitude = 52.401;
-float longitude = 16.902;
+float latitude;
+float longitude;
 int battery;
 
 // Create BME280 object
@@ -70,46 +106,59 @@ BME280_I2C bme(0x76); // I2C using address 0x76
 // Create NovaSDS011 object
 NovaSDS011 sds011;
 // Create SoftwareSerial object
-SoftwareSerial EspSerial(ESP_PIN_RX, ESP_PIN_TX);
+SoftwareSerial WWWSerial(WWW_PIN_RX, WWW_PIN_TX);
 
 void setup()
 {
+
     analogReference(EXTERNAL);
 
     bme.begin();
     sds011.begin(SDS_PIN_RX, SDS_PIN_TX);
     Serial.begin(9600);
-    EspSerial.begin(9600);
-
-    resetWiFi(2000);
-    connectWiFi(5000);
+    WWWSerial.begin(9600);
+    Serial.println("end setup");
 }
 
 void loop()
 {
 
-    memoryCheck();
+    if (DEBUG)
+    {
+        memoryCheck();
+    }
 
     readSensors();
 
-    startTCPconnection(1000);
-
-    sendOverTCP(3000);
-
-    delay(100000);
+    if (GSM_MODE)
+    {
+        testGSM(1000);
+        GSM_location(2000);
+        startTCPconnectionGSM(2000);
+        sendOverTCP(2000);
+        sendData(readFromFlash(cip_exit_config) + r + n, 1000, DEBUG);
+    }
+    else
+    {
+        resetWiFi(2000);
+        connectWiFi(7000);
+        startTCPconnection(1000);
+        sendOverTCP(2000);
+    }
+    delay(3600000);
 }
 
 void sendData(String command, int timeout, boolean debug)
 {
-    EspSerial.print(command);
+    WWWSerial.print(command);
     unsigned long time = millis();
     while ((time + timeout) > millis())
     {
         if (debug)
         {
-            while (EspSerial.available())
+            while (WWWSerial.available())
             {
-                char c = EspSerial.read();
+                char c = WWWSerial.read();
                 delay(1);
                 Serial.print(c);
                 if (c == '\0')
@@ -117,6 +166,66 @@ void sendData(String command, int timeout, boolean debug)
             }
         }
     }
+}
+
+void sendDataAnswer(String command, int timeout, boolean debug)
+{
+    WWWSerial.print(command);
+    unsigned long time = millis();
+    String data;
+    while ((time + timeout) > millis())
+    {
+        if (debug)
+        {
+            while (WWWSerial.available())
+            {
+                char c = WWWSerial.read();
+                delay(1);
+                Serial.print(c);
+                data = data + c;
+                if (c == '\0')
+                    continue;
+            }
+        }
+    }
+    if (data.charAt(data.indexOf(':') + 2) == '0')
+    {
+        data = data.substring(data.indexOf(':') + 4, data.lastIndexOf(','));
+        latitude = data.substring(data.indexOf(',') + 1).toFloat();
+        longitude = data.substring(0, data.indexOf(',')).toFloat();
+    }
+}
+
+void testGSM(int timeout)
+{
+    sendData(readFromFlash(handshake) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(signal_test) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(network_test) + r + n, timeout, DEBUG);
+}
+void GSM_location(int timeout)
+{
+    sendData(readFromFlash(close_bearer) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(cip_exit_config) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(attach_GPRS) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(set_bearer_profile) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(set_bearer_apn) + readFromFlash(gprs_apn) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(open_bearer) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(query_bearer) + r + n, timeout, DEBUG);
+    sendDataAnswer(readFromFlash(get_location) + r + n, 2 * timeout, DEBUG);
+    sendData(readFromFlash(close_bearer) + r + n, timeout, DEBUG);
+}
+void startTCPconnectionGSM(int timeout)
+{
+    sendData(readFromFlash(cip_status) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(cip_mux) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(set_PDP) + readFromFlash(gprs_apn) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(cip_apn) + readFromFlash(gprs_apn) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(cip_gprs) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(cip_ip) + r + n, timeout, DEBUG);
+    sendData(readFromFlash(cip_status) + r + n, timeout, DEBUG);
+
+    String ATcommand = readFromFlash(start_TCP_connection) + readFromFlash(server) + "\"," + String(port) + r + n;
+    sendData(ATcommand, timeout, DEBUG);
 }
 
 void connectWiFi(int timeout)
@@ -150,40 +259,34 @@ void sendOverTCP(int timeout)
     sendDataLength = sendDataLength + JSONdataLength_2();
     sendDataLength = sendDataLength + JSONdataLength_3();
     sendDataLength = sendDataLength + JSONdataLength_4();
+    sendDataLength = sendDataLength + JSONdataLength_5();
 
     sendData(readFromFlash(send_over_TCP) + String(sendDataLength) + r + n, timeout, DEBUG);
 
     //send body in parts
-    EspSerial.print(StringPOSTbody_1());
-    EspSerial.print(StringPOSTbody_2());
-    EspSerial.print(StringPOSTbody_3());
-    EspSerial.print(StringPOSTbody_4());
-    EspSerial.print(StringJSON_1());
-    EspSerial.print(StringJSON_2());
-    EspSerial.print(StringJSON_3());
-    EspSerial.print(StringJSON_4());
+    WWWSerial.print(StringPOSTbody_1());
+    WWWSerial.print(StringPOSTbody_2());
+    WWWSerial.print(StringPOSTbody_3());
+    WWWSerial.print(StringPOSTbody_4());
+    WWWSerial.print(StringJSON_1());
+    WWWSerial.print(StringJSON_2());
+    WWWSerial.print(StringJSON_3());
+    WWWSerial.print(StringJSON_4());
+    WWWSerial.print(StringJSON_5());
     unsigned long time = millis();
     while ((time + (2 * timeout)) > millis())
     {
         if (DEBUG)
         {
-            while (EspSerial.available())
+            while (WWWSerial.available())
             {
-                char c = EspSerial.read();
+                char c = WWWSerial.read();
                 delay(1);
                 Serial.print(c);
                 if (c == '\0')
                     continue;
             }
         }
-    }
-}
-
-void wait(int timeout)
-{
-    unsigned long time = millis();
-    while ((time + timeout) > millis())
-    {
     }
 }
 
@@ -267,8 +370,8 @@ byte readUV()
 
 int readBattery()
 {
-    word value = analogRead(BATTERY);
-    return (int)(value * 5000 / 1024);
+    int value = analogRead(BATTERY);
+    return (int)(((value * 5.0) / 1024.0) * 1000);
 }
 
 void readSDS()
@@ -301,6 +404,17 @@ String StringJSON_3()
 }
 String StringJSON_4()
 {
+    char buff_latitude[10];
+    char buff_longitude[10];
+    dtostrf(latitude, 4, 4, buff_latitude);
+    dtostrf(longitude, 4, 4, buff_longitude);
+    String JSONdata =
+        readFromFlash(JSON_latitude) + buff_latitude +
+        readFromFlash(JSON_longitude) + buff_longitude;
+    return JSONdata;
+}
+String StringJSON_5()
+{
     String JSONdata =
         readFromFlash(JSON_device_name) + readFromFlash(device_name) +
         readFromFlash(JSON_server_password) + readFromFlash(server_password) + "\"}";
@@ -322,6 +436,10 @@ int JSONdataLength_3()
 int JSONdataLength_4()
 {
     return StringJSON_4().length();
+}
+int JSONdataLength_5()
+{
+    return StringJSON_5().length();
 }
 
 String StringPOSTbody_1()
@@ -346,7 +464,7 @@ String StringPOSTbody_3()
 }
 String StringPOSTbody_4()
 {
-    int JSONdataLength = (JSONdataLength_1() + JSONdataLength_2() + JSONdataLength_3() + JSONdataLength_4());
+    int JSONdataLength = (JSONdataLength_1() + JSONdataLength_2() + JSONdataLength_3() + JSONdataLength_4() + JSONdataLength_5());
     String POSTbody =
         readFromFlash(HTML_POST_header_5) + JSONdataLength + r + n + r + n; //"Content-Length: "
     return POSTbody;
